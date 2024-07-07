24c5ebe30adfd656587885239ff7b6b19d58dc2e
Mahesh <maheshp8767@gmail.com>
2024-07-07 19:27:27 +0530

    timeout solve

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,40 @@
+// import mongoose from "mongoose";
+// import dotenv from "dotenv"
+// dotenv.config({
+//     path:"../config/.env"
+// })
+
+
+// const databaseConnection = ()=> {
+//  mongoose.connect(process.env.MONGO_URI).then(()=>{
+//     console.log('connected to MongoDB databse')
+//  }).catch((error)=>{
+//     console.log(error);
+//  })
+// }
+
+// export default databaseConnection;
+
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+
 dotenv.config({
-    path:"../config/.env"
-})
+  path: "../config/.env"
+});
 
-
-const databaseConnection = ()=> {
- mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log('connected to MongoDB databse')
- }).catch((error)=>{
-    console.log(error);
- })
-}
+const databaseConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout for server selection
+      socketTimeoutMS: 45000, // 45 seconds timeout for socket operations
+    });
+    console.log('Connected to MongoDB database');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit with failure
+  }
+};
 
 export default databaseConnection;
